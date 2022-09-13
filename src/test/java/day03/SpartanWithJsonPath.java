df6b924f88b73c9cfdb5dba24131294daf0e33ff
Suna Kara <karacalisuna912@gmail.com>
2022-09-13 21:52:52 +0300

    test2 is created

--- src/test/java/day03/SpartanWithJsonPath.java
+++ src/test/java/day03/SpartanWithJsonPath.java
@@ -1,5 +1,51 @@
 package day03;
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import utilities.ConfigurationReader;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 public class SpartanWithJsonPath {
+
+    @BeforeAll
+    public static void  init(){
+        baseURI=ConfigurationReader.getProperty("sp_url");
+    }
+
+    @Test
+    public  void  test1(){
+
+        Response response = given().accept(ContentType.JSON).and().pathParam("id", 15)
+                .when().get("/api/spartans/{id}");
+
+        response.prettyPrint();
+
+        JsonPath jsonPath=response.jsonPath();
+        System.out.println("jsonPath.getInt(\"id\") = " + jsonPath.getInt("id"));
+
+        System.out.println("jsonPath.getString(\"name\") = " + jsonPath.getString("name"));
+        System.out.println("jsonPath.getLong(\"phone\") = " + jsonPath.getLong("phone"));
+        System.out.println("jsonPath.getString(\"gender\") = " + jsonPath.getString("gender"));
+
+        assertEquals(200,response.statusCode());
+        assertEquals("application/json",response.contentType());
+        assertEquals("Meta",jsonPath.getString("name"));
+        assertEquals("Female",jsonPath.getString("gender"));
+        assertEquals(15,jsonPath.getInt("id"));
+        assertEquals(1938695106,jsonPath.getLong("phone"));
+    }
+
+    @Test
+    public void test2(){
+
+    }
 
 }
